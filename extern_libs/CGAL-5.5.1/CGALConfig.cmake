6ab82788bd51c5fd482cd87ed08274da5df8ac88
Edin Spiegel <apps4trainers@gmail.com>
2023-02-23 21:19:35 +0200

    initial commit

---
+++ extern_libs/CGAL-5.5.1/CGALConfig.cmake
@@ -0,0 +1,5 @@
+# 
+# This file points to the CGALConfig.cmake for header-only CGAL.
+#
+
+include(/home/edin/Programs/CGAL-5.5.1/lib/cmake/CGAL/CGALConfig.cmake)
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import system_theme
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SystemThemePlugin.register(with: registry.registrar(forPlugin: "SystemThemePlugin"))
+}
